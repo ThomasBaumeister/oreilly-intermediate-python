2737d795b334f4afe03e37cbac163ae14612aa51
Thomas Baumeister <Thomas@baumeister-web.de>
2017-10-24 00:34:35 +0200

    fixed

--- Unit4/jeopardy/simple.py
+++ Unit4/jeopardy/simple.py
@@ -2,11 +2,13 @@
 
 import sqlite3
 
-connection=sqlite3.connect("jepordy.db")
+connection=sqlite3.connect("jeopardy.db")
+
+print (connection)
 
 cursor=connection.cursor()
 
-cursor.execute("select name from category limit 10")
+cursor.execute("select text from clue limit 10")
 
 for category in cursor.fetchall():
 	print (category[0])
